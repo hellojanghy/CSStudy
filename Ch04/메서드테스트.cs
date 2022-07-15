3f5950a28eca4005633e3bf880f69eb4bd7b1f56
helojanghy <hyello@kakao.com>
2022-07-15 15:01:31 +0900

    2022/07/15 수업예제

---
+++ Ch04/메서드테스트.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch04
+{
+    internal class 메서드테스트
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("학원에서 작업");
+        }
+    }
+}
