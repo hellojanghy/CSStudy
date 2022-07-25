7584e75755175823935bcecd5489918edf4b7195
helojanghy <hyello@kakao.com>
2022-07-25 11:46:32 +0900

    2022/07/25 수업예제

---
+++ Ch07/3_List.cs
@@ -0,0 +1,119 @@
+﻿using Ch07.Sub1;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 날짜 : 2022/07/25
+ * 이름 : 장혜영
+ * 내용 : 컬렉션 리스트 실습하기 교재 p211
+ * 
+ *  리스트(List)
+ *   - 배열과 유사한 선형자료구조로 배열과 다르게 동적으로 데이터를 처리
+ *   - ArrayList를 일반화 시켜 더 나은 성능을 가진 List를 사용
+ */
+namespace Ch07
+{
+    internal class _3_List
+    {
+        static void Main1(string[] args)
+        {
+            ///////////////////////////
+            /// ArrayList
+            ///////////////////////////
+           
+            
+            // ArrayList 생성
+            ArrayList arrList1 = new ArrayList();
+
+            // 데이터 추가
+            arrList1.Add(1);
+            arrList1.Add(2);
+            arrList1.Add(3);
+            arrList1.Add(4);
+            arrList1.Add(5);
+
+            // 데이터 삽입
+            arrList1.Insert(1, 6); // 1,2,3,4,5 > 1,6,2,3,4,5 이렇게 댐
+                
+            // 데이터 삭제
+            arrList1.Remove(6); //6번째 칸을 삭제하는 게 아님 6을 지우는 거
+            arrList1.RemoveAt(0); //이건 칸 맞음
+
+            // 데이터 출력
+            foreach (int n in arrList1)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+
+            // 다양한 타입의 데이터를 갖는 ArrayList
+            ArrayList arrList2 = new ArrayList();
+            arrList2.Add(1);
+            arrList2.Add(1.23);
+            arrList2.Add(true);
+            arrList2.Add('A');
+            arrList2.Add("Apple");
+
+
+            for (int i = 0; i <= arrList2.Count ; i++)
+            {
+                Console.Write(arrList2[i] + " ");
+            }
+
+            Console.WriteLine();
+            ///////////////////////////
+            /// List
+            ///////////////////////////
+
+            // List 생성
+            List<int> list1 = new List<int>();
+
+            // 데이터 추가
+            list1.Add(1);
+                list1.Add(2);
+                list1.Add(3);
+                list1.Add(4);
+                list1.Add(5);
+
+            // 데이터 삽입
+            list1.Insert(1, 6);
+
+            // 데이터 삭제
+            arrList1.Remove(4); 
+            arrList1.RemoveAt(1);
+
+            // 데이터 출력
+            foreach (int n in list1)
+            {
+                Console.Write(n + " ");
+            }
+
+            List<string> list2 = new List<string>();
+            list2.Add("서울");
+            list2.Add("대전");
+            list2.Add("대구");
+            list2.Add("부산");
+            list2.Add("광주");
+
+            foreach (string city in list2)
+            {
+                Console.Write(city + " ");
+            }
+
+            //List<Apple> list3 = new<Apple>();
+
+            //list3.Add(new Apple("한국", 3000));
+            //list3.Add(new Apple("일본", 2000));
+            //list3.Add(new Apple("미국", 1000));
+
+            //Apple apple = List(3[0]);
+            //apple.Show();
+
+            //list3[2].Show();
+            //list3[3].Show();
+        }
+    }
+}
---
+++ Ch07/5_Hashset.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 날짜 : 2022/07/25
+ * 이름 : 장혜영
+ * 내용 : 컬렉션 딕셔너리 실습하기
+ * 
+ * 집합(Hashset)
+ *  - 저장되니 데이터의 순서를 고려하지 않고, 중복을 허용하지 않는 자료구조
+ *  - 중복된 데이터를 제거하거나 이미 데이터가 존재하는지 검사에 활용
+ */
+namespace Ch07
+{
+    internal class _5_Hashset 
+    {
+        static void Main(string[] args)
+        {
+            // 집합 생성
+            HashSet<int> set = new HashSet<int>();
+
+            // 데이터 추가
+            set.Add(1);
+            set.Add(2);
+            set.Add(3);
+            set.Add(4);
+            set.Add(5);
+            set.Add(2);
+            set.Add(3);
+
+            // 데이터 출력
+            Console.WriteLine("집합 갯수 :" + set.Count);
+
+            foreach (int n in set)
+            {
+                Console.WriteLine(n +" ");
+            }
+            Console.WriteLine();
+
+            // 집합 연산
+            HashSet<int> set1 = new HashSet<int>() { 1, 2, 3, 4, 5 };
+            HashSet<int> set2 = new HashSet<int>() { 2, 3, 5, 6, 7 };
+
+            var result1 = set1.Intersect(set2);  // 교집합 { 2, 3, 5 }
+            var result2 = set1.Union(set2);      // 합집합 { 1, 2, 3, 4, 5, 6, 7 }
+            var result3 = set1.Except(set2);     // 차집합 { 1, 4 }
+
+            Console.WriteLine("result1 :");
+            foreach (int n in result1)
+            {
+                Console.Write(n+" ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("result2 :");
+            foreach (int n in result2)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("result3 :");
+            foreach (int n in result3)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
